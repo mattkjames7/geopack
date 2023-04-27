10d8f664ae3c48b8b4438b1fc4c617b64bacaf33
Matt <mattkjames7@gmail.com>
2023-04-27 16:40:27 +0100

    updated some batch files

---
+++ compile.bat
@@ -0,0 +1,16 @@
+@echo off
+echo Compiling datetime
+cd lib\datetime
+call compile.bat
+cd ..\..
+copy lib\datetime\lib\libdatetime.dll lib\
+
+echo Compiling libspline
+cd lib\libspline
+call compile.bat
+cd ..\..
+copy lib\libspline\lib\libspline.dll lib\
+
+cd src
+call compile.bat
+cd ..
